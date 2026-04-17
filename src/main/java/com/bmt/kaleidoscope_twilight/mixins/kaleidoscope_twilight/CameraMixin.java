54bde9cc422378330cef6a3c11cd04a8f86e27bc
BmtUltra <1453935889@qq.com>
2026-04-17 21:18:38 +0800

    暮色酒

--- src/main/java/com/bmt/kaleidoscope_twilight/mixins/kaleidoscope_twilight/CameraMixin.java
+++ src/main/java/com/bmt/kaleidoscope_twilight/mixins/kaleidoscope_twilight/CameraMixin.java
@@ -1,11 +1,9 @@
-package com.bmt.kaleidoscope_twilight.mixins;
+package com.bmt.kaleidoscope_twilight.mixins.kaleidoscope_twilight;
 
 import com.bmt.kaleidoscope_twilight.init.KTEffects;
 import net.minecraft.client.Camera;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.phys.HitResult;
-import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
