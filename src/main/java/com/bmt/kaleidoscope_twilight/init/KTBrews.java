0abb72f5002c5f5a445693c85de0f2c321e3a7bc
BmtUltra <1453935889@qq.com>
2026-06-16 13:23:41 +0800

    补充模型

--- src/main/java/com/bmt/kaleidoscope_twilight/init/KTBrews.java
+++ src/main/java/com/bmt/kaleidoscope_twilight/init/KTBrews.java
@@ -36,7 +36,7 @@ public class KTBrews {
     
     // 巫术秘酿
     public static final DeferredBlock<Block> WITCHCRAFT_SECRET_BREW = BLOCKS.register("witchcraft_secret_brew", 
-            () -> DrinkBlock.create().maxCount(4).shapes(
+            () -> DrinkBlock.create().maxCount(4).irregular().shapes(
                     Block.box(4, 0, 4, 12, 15, 12),
                     Block.box(0, 0, 4, 16, 15, 12),
                     Shapes.or(
@@ -48,7 +48,7 @@ public class KTBrews {
     
     // 蛇蜕利
     public static final DeferredBlock<Block> SNAKE_SKIN_LIQUOR = BLOCKS.register("snake_skin_liquor", 
-            () -> DrinkBlock.create().maxCount(4).shapes(
+            () -> DrinkBlock.create().maxCount(4).irregular().shapes(
                     Block.box(4, 0, 4, 12, 15, 12),
                     Block.box(0, 0, 4, 16, 15, 12),
                     Shapes.or(
@@ -60,7 +60,7 @@ public class KTBrews {
     
     // 冰晶露
     public static final DeferredBlock<Block> ICE_CRYSTAL_FROST_DEW = BLOCKS.register("ice_crystal_frost_dew", 
-            () -> DrinkBlock.create().maxCount(2).shapes(
+            () -> DrinkBlock.create().maxCount(2).irregular().shapes(
                     Block.box(4, 0, 4, 12, 15, 12),
                     Block.box(0, 0, 4, 16, 15, 12),
                     Shapes.or(
@@ -104,7 +104,7 @@ public class KTBrews {
     
     // 荆棘之心
     public static final DeferredBlock<Block> THORN_HEART = BLOCKS.register("thorn_heart", 
-            () -> DrinkBlock.create().maxCount(2).shapes(
+            () -> DrinkBlock.create().maxCount(2).irregular().shapes(
                     Block.box(4, 0, 4, 12, 15, 12),
                     Block.box(0, 0, 4, 16, 15, 12),
                     Shapes.or(
@@ -128,7 +128,7 @@ public class KTBrews {
     
     // 辉夜鸟之歌
     public static final DeferredBlock<Block> GLOWING_NIGHT_BIRD_SONG = BLOCKS.register("glowing_night_bird_song", 
-            () -> DrinkBlock.create().maxCount(2).shapes(
+            () -> DrinkBlock.create().maxCount(2).irregular().shapes(
                     Block.box(4, 0, 4, 12, 15, 12),
                     Block.box(0, 0, 4, 16, 15, 12),
                     Shapes.or(
